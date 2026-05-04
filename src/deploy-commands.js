0cade1d6535b27893dc2aef40693abda94a78d9e
Ramanand <ramanandshirbhate@gmail.com>
2026-05-04 09:33:49 +0530

    Update deploy-commands.js

--- src/deploy-commands.js
+++ src/deploy-commands.js
@@ -21,6 +21,6 @@ const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
     console.log('Successfully reloaded application (/) commands.');
   } catch (error) {
     console.error('Error loading commands:', error);
-    process.exit(1);
+    process.exitCode = 1;
   }
 })();
